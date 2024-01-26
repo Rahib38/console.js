bcfa131de26a25cb23d6f96d45a936a4121c785f
Rahib38 <nadimulrahib38@gmail.com>
2024-01-26 12:21:12 -0800

    create a js file

---
+++ task1.js
@@ -0,0 +1,6 @@
+var apples = 300;
+var oranges = 400;
+var totalBuy = apples + oranges;
+var giveMoney = 1000;
+var returnMoney = giveMoney - totalBuy;
+console.log (returnMoney)
---
+++ task2.js
@@ -0,0 +1,8 @@
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var total = math + biology + chemistry + physics + bangla;
+var theAverageMarks = total / 5;
+console.log(theAverageMarks.toFixed(2));
