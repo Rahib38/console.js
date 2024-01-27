c3036584361ee8901e641f08fcff5a7a1a580d0a
Rahib38 <nadimulrahib38@gmail.com>
2024-01-26 22:48:46 -0800

    added a nested js

---
+++ nested.js
@@ -0,0 +1,18 @@
+const money = 500;
+if (money > 300){
+    console.log('you are  rich');
+}
+else{
+    if(money > 100){
+        console.log('tui borolok o na gorib o na');
+    }
+    else{
+        if(money > 0){
+            console.log('kola kha');
+        }
+        else{
+            console.log("tui amar bondu na")
+        }
+    }
+
+}
