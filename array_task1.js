40e2f739f12b44b6cdfdbf6d3051e858f5e94f62
Rahib38 <nadimulrahib38@gmail.com>
2024-01-28 11:11:18 -0800

    added comment

--- array_task1.js
+++ array_task1.js
@@ -1,4 +1,5 @@
+// Declare an array
 const fruits = ['apple', 'orange', 'banana', 'watermelon', 'lemon'];
-console.log(fruits.indexOf('watermelon'));
-fruits[2] = 'jambura';
+console.log(fruits.indexOf('watermelon'));  //console log index 3rd elements
+fruits[2] = 'jambura';  //change the value of the 2nd index element to jambura
 console.log(fruits);
