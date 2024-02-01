b8c278b5a4cd09e97408830c4f452f8f08635f39
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-01 13:48:59 -0800

    added a case

---
+++ case.js
@@ -0,0 +1,15 @@
+const sub = 'Chemistry';
+const book = 'chemistry';
+if (sub.toLowerCase()===book.toLowerCase()){
+    console.log("pass kormu");
+}else{
+    console.log('fail kormu');
+}
+
+// const drink = 'water';
+// const liquied = '    water';
+// if(drink.trim === liquied.trim){
+//     console.log('panir opr nam jibon');
+// }else{
+//     console.log('sea er pani kha');
+// }
