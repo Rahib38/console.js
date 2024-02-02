171990497298a1483eda3d57d1755f70d001460c
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-01 22:26:05 -0800

    added a split

--- slice.js
+++ slice.js
@@ -1,3 +1,8 @@
 const address = 'noakhali';
 const part = address.slice(2,5);
 console.log(part);
+
+// split
+const sentence = 'I am a good boy';
+console.log (sentence.split());
+console.log (sentence.split(' '));
