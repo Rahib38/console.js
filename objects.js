938311fde170cd82087dcb08410dbd81a0c7a20c
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-01 23:47:23 -0800

    added a objects

---
+++ objects.js
@@ -0,0 +1,9 @@
+const person = {
+    name : 'Solim',
+    age :25,
+    profession :'developer',
+    salary :25000
+}
+console.log(person);
+console.log(person.salary);
+console.log(person['age']);
