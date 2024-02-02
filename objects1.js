1b274057eea50be3bead1365c126a4580c1461d0
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-02 01:07:17 -0800

    added a objects keys and value

---
+++ objects1.js
@@ -0,0 +1,10 @@
+const computer = {
+    brand :' gigabyte',
+    price : 35000,
+    monitor : 'unv',
+    ssd : 512
+}
+const keys = Object.keys(computer);
+console.log(keys);
+const value = Object.values(computer);
+console.log(value);
