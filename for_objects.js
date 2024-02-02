41d867263f9300dad9ceef9fbab76765692f42ef
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-02 01:15:43 -0800

    added a for objects

---
+++ for_objects.js
@@ -0,0 +1,13 @@
+const computer = {
+    brand :' gigabyte',
+    price : 35000,
+    monitor : 'unv',
+    ssd : 512
+}
+for (const keys in computer){
+    console.log(keys);
+}
+for (const keys in computer){
+    console.log(keys);
+    console.log(computer[keys]);
+}
