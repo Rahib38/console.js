db12a02cfadf0133824154c7f3d292fbdc33dc37
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-07 11:51:05 -0800

    added a for

--- for.js
+++ for.js
@@ -1,4 +1,9 @@
-const nums = [20, 5, 55, 10, 15, 30];
-for (let i = 0; i<nums.length-1;i++){
-    console.log (nums[i]);
+// const nums = [20, 5, 55, 10, 15, 30];
+// for (let i = 0; i<nums.length-1;i++){
+//     console.log (nums[i]);
+// }
+
+let i = 1;
+for (i =1; i<=100;i++){
+    console.log("I Love you sona pakhi", i);
 }
