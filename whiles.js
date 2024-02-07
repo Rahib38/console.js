0ed7d11bfd21a03711562f63b47dd228922a368f
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-07 11:07:44 -0800

    added a whiles

---
+++ whiles.js
@@ -0,0 +1,17 @@
+// sum
+let i = 1;
+let sum = 0;
+while (i<= 10){
+    sum += i;
+    i++;
+}
+console.log(sum);
+
+// mul
+let j = 1;
+let mul = 1;
+while (j<= 10){
+    mul *= j;
+    j++;
+}
+console.log(mul);
