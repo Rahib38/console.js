dde59fe33352db13621ce4589f901985a7c9cf9d
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-06 19:10:31 -0800

    added a function

---
+++ fun.js
@@ -0,0 +1,5 @@
+function print (name, age , study){
+    return`My name is ${name}. My age is ${age}. My study is${study}`
+}
+const x = print ('Nadimul Rahib', 20, ' CST at diploma enginnering');
+console.log(x);
