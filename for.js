b68ec9ee922b69c253ce6210d5c0d6d7ebc0493e
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-09 18:39:45 -0800

    added  a for.js

--- for.js
+++ for.js
@@ -3,7 +3,23 @@
 //     console.log (nums[i]);
 // }
 
-let i = 1;
-for (i =1; i<=100;i++){
-    console.log("I Love you sona pakhi", i);
+// let i = 1;
+// for (i =1; i<=100;i++){
+//     console.log("I Love you sona pakhi", i);
+// }
+
+let nums = [20, 5, 55, 10, 15, 30];
+let i = 0;
+let sumOfEven = 0;
+let sumOfOdd = 0;
+for (i=0;i<nums.length;i++){
+    const j = nums[i];
+    if(j%2===1){
+        sumOfOdd+=j;
+    }else{
+        sumOfEven+=j;
+
+    }
 }
+console.log(sumOfEven);
+console.log(sumOfOdd);
