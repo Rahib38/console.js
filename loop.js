96a8b1f502ef99f66e688d63991458d924032851
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-09 19:58:45 -0800

    added a loop

---
+++ loop.js
@@ -0,0 +1,11 @@
+const products = [
+    {id:1, name:'xiami phone one night', price:19000},
+    {id:2, name:'iphone', price:119000},
+    {id:3, name:'mac book air', price:219000},
+    {id:4, name:'lenovo yoga', price:19000},
+    {id:5, name:'samsung note 1', price:19000},
+    {id:6, name:'phone one', price:19000},
+];
+for (const product of products){
+    console.log(product);
+}
