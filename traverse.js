44ca558165929a54330c4c1f3333e176abd74e46
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-23 10:00:16 -0800

    added a traverse

---
+++ traverse.js
@@ -0,0 +1,10 @@
+let sum = 0;
+const arr = [20, 15, 52, 7, 95, 35, 48, 9];
+for (const array of arr){
+    // console.log(array);
+    // sum += array;
+    if (array % 2 === 0){
+        console.log(array);
+    }
+}
+// console.log(sum);
